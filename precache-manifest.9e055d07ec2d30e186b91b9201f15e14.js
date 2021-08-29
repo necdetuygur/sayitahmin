b11d4a35588f9ea9fd1d88aa48a273add40e91d1
Necdet UYGUR <necdet.uygur@gmail.com>
2021-08-30 02:13:23 +0300

    New Deployment

---
+++ precache-manifest.9e055d07ec2d30e186b91b9201f15e14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d28e34404ee2963b258",
+    "url": "https://necdetuygur.github.io/sayitahmin/css/app.1aac2755.css"
+  },
+  {
+    "revision": "bab35b03c775ea5e6c06",
+    "url": "https://necdetuygur.github.io/sayitahmin/css/chunk-vendors.63c5e34a.css"
+  },
+  {
+    "revision": "b1fb0395bb9c158424480fe423fa7b33",
+    "url": "https://necdetuygur.github.io/sayitahmin/index.html"
+  },
+  {
+    "revision": "2d28e34404ee2963b258",
+    "url": "https://necdetuygur.github.io/sayitahmin/js/app.e60f18d9.js"
+  },
+  {
+    "revision": "bab35b03c775ea5e6c06",
+    "url": "https://necdetuygur.github.io/sayitahmin/js/chunk-vendors.db8ec49d.js"
+  },
+  {
+    "revision": "21392c808639041f85c16704221a1d82",
+    "url": "https://necdetuygur.github.io/sayitahmin/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "https://necdetuygur.github.io/sayitahmin/robots.txt"
+  }
+]);
